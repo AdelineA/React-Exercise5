c718174a699641922f44c732c85a337b6852e539
Adeline Ayinkamiye <adelinemug6@gmail.com>
2023-10-05 14:25:03 +0200

    add list

--- src/components/List.js
+++ src/components/List.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import ListItems from './ListItems';
+import React from "react";
+import ListItems from "./ListItems";
 
 const animals = [
-    { name: "dog", head: "🐕" },
-    { name: "cat", head: "🐈" },
-    { name: "chicken", head: "🐕" },
-    { name: "cow", head: "🐕" },
-    { name: "sheep", head: "🐕" },
-    { name: "horse", head: "🐕" },
-  ];
-  
+  { head: "🐕", name: "dog" },
+  { head: "🐈", name: "cat" },
+  { head: "🐤", name: "chicken" },
+  { head: "🐄", name: "cow" },
+  { head: "🐑", name: "sheep" },
+  { head: "🐎", name: "horse" },
+];
+
 export default function List() {
   return (
     <div className="py-10 flex flex-col items-center justify-center">
-    <ul className="list-disc">
-      {animals.map((animal, index) => (
-        <ListItems key = {index} name={animal.name} head={animal.head} />
-      ))}
-    </ul>
-  </div>
-  )
+      <ul className="list-disc">
+        {animals.map((animal, index) => (
+          <ListItems key={index} name={animal.name} head={animal.head} />
+        ))}
+      </ul>
+    </div>
+  );
 }
