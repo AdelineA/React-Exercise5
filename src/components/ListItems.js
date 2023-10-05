80f93f57e3384c2748460b4e7afe059c6021441b
Adeline Ayinkamiye <adelinemug6@gmail.com>
2023-10-05 14:32:39 +0200

    add head

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function ListItems(props) {
   return (
     <li className="list-none px-4 py-3 rounded bg-blue-100">
-      {props.name} {props.head}
+       {props.head}{props.name}
     </li>
   );
 }
